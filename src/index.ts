db1849541e64e6ba1de94ebecf351f4f9d33f714
RenatoAlexandrini <re.alexandrini@gmail.com>
2023-04-06 18:08:47 -0300

    projeto labecommerce

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import {app} from './app'
-import {postUserAdd} from './endpoints/postUsersAdd'
-import {getAllUsers} from './endpoints/getAllUsers'
-import {postProductAdd} from './endpoints/postProductAdd'
-import {getAllProducts} from './endpoints/getAllProducts'
+import { app } from './app'
+import { postUserAdd } from './endpoints/postUsersAdd'
+import { getAllUsers } from './endpoints/getAllUsers'
+import { postProductAdd } from './endpoints/postProductAdd'
+import { getAllProducts } from './endpoints/getAllProducts'
 import { postPurchaseAdd } from './endpoints/postPurchaseAdd'
-import {getUserPurchases} from './endpoints/getUserPurchases'
+import { getUserPurchases } from './endpoints/getUserPurchases'
 
 app.post('/users', postUserAdd)
 
 app.get('/users', getAllUsers)
 
-app.post('/products', postProductAdd )
+app.post('/products', postProductAdd)
 
 app.get('/products', getAllProducts)
 
